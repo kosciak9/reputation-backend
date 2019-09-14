d0d9ee9c1079473b1c7ef39d5a16d6a6ade3c432
Patryk Fryda <patrykfryda@gmail.com>
2019-09-14 20:52:52 +0200

    minor user changes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,32 +1,43 @@
 class UsersController < ApplicationController
   def index
     @users = User.all
-    json_response(@users)
+    render json: @users
   end
 
   def create
-    @user = User.create!(user_params)
-    json_response(@user,:created)
+    @user = User.create(user_params)
+
+    if @user.save
+      render json: @user, status: :created
+    else
+      render json: @user.errors, status: :unprocessable_entity
+    end
   end
 
   def show
-    json_response(@user)
+    render json: @user
   end
 
   def update
-    @user.update(todo_params)
-    head :no_ontent
+    if @user.update(user_params)
+      render json: @user
+    else
+      render json: @user.errors, status: :unprocessable_entity
+    end
   end 
 
   def destroy
     @user.destroy
-    head :no_content
   end
 
   private
 
   def set_user
     @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:reputation_score, :name, :age, :password, :login, :email)
   end
 
                   
